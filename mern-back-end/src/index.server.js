1cf2aae868393a0bf4ae5b2274a5949449e0d2c3
hareesh-r <hareeshprogrammer@gmail.com>
2021-08-14 12:02:34 +0530

    mongodb connected

--- mern-back-end/src/index.server.js
+++ mern-back-end/src/index.server.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
 const env = require('dotenv');
+const mongoose = require('mongoose');
 
 env.config();
+
+//MongoDB Connection
+//mongodb+srv://root:<password>@cluster0.bxg8u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+mongoose.connect(
+    `mongodb+srv://${process.env.MONGO_DB_ADMIN}:${process.env.MONGO_DB_PASSWORD}@cluster0.bxg8u.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+).then(() => {
+    console.log('MongoDB Connection Successful');
+});
+
 
 app.use(express.json());
 
---
+++ mern-back-end/src/routes/user.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/signin', (req, res) => {
+
+});
+
+router.post('/signup', (req, res) => {
+
+});
+
+module.exports = router;
